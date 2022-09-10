f5e6fdd1d3f47e51df30948f0a54164f4b445a15
Akihiro Tojo <akihiro.021109@gmail.com>
2022-09-10 16:28:37 +0900

    Update style

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import Logo from './components/Logo';
 import { LOGO } from './constant';
 
-export const SApp = styled.div`
+const SApp = styled.div`
   max-width: 1280px;
   margin: 0 auto;
   padding: 2rem;
